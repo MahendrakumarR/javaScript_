c04ad55812297dfd82319ce62d24d54b7406bdfa
MahendraN <mahendraramalingam8056@gmail.com>
2023-12-31 20:28:07 +0530

    update

--- js/dom.js
+++ js/dom.js
@@ -36,19 +36,35 @@ evendiv[3].style.backgroundColor = "purple"; // here 'evendiv[3]' index position
 
 // ============ change all even elements color/width/properties ============================
 
-for(i=0; i<evendiv.length; i++){
+for(i=0; i<evendiv.length; i++){          // here I want all even numbers properties change
     evendiv[i].style.width = "150px";                  // increase height
     evendiv[i].style.height = "150px";                // increase width
     evendiv[i].style.color = "yellow";               // this is a number color 
     evendiv[i].style.backgroundColor = "blue";    // change an element backgroundcolor 
-
+   
 }
 
 // ============ change the 'nav' inside text ================================================
 
-const change_text_name = document.querySelectorAll("nav h1");   // here using h1 for which size already used
-change_text_name.innerHTML = '<h1> Welcome to my page</h1>';
+const change_text_name = document.querySelector("nav h1");         // here using h1 for which size already used
+change_text_name.textContent = 'Welcome to my page';              //  here change text content
+change_text_name.innerHTML= '<h1> Welcome to my page2 </h1>';    // here change nside the html code
+ 
+console.log(evendiv[0].parentElement);                              // select the section whole(odd and even elements)
+console.log(evendiv[0].parentElement.children);                    // select section inside 'odd' and 'even' elements
+console.log(evendiv[0].parentElement.childNodes);                 // now nodes(25) becacuse " 'enter' or '\n' " and 'div' both can take
+console.log(evendiv[0].parentElement.hasChildNodes());           // check there is a child node there
+console.log(evendiv[0].parentElement.lastElementChild);         // last child is 'enter' or '\n'
+// or 
+console.log(evendiv[0].parentElement.firstElementChild);       // first child is " 'enter' or '\n' " or 'div'
+console.log(evendiv[0].nextSibling);                          // next sibling is 'enter or \n' 
+console.log(evendiv[0].nextElementSibling);                  // next element sibling is '3' 
+// or
+console.log(evendiv[0].previousElementSibling);            // previous element sibling is '1' 
 
+ 
+
+     
 
 
 
