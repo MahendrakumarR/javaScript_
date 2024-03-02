cd13cf5c0973b96ba14a2c053ef0b183a7535681
MahendraN <mahendraramalingam8056@gmail.com>
2024-03-02 15:12:38 +0530

    update

--- js/event_listener.js
+++ js/event_listener.js
@@ -2,7 +2,7 @@ const view = document.querySelector("#view2");
 console.log(view);
 
 const div = view.querySelector("div");
-const h2 = div.querySelector("h2")
+const h2 = div.querySelector("h2");
 
 //   syntax
 // .addEventListeners(event, function, useCapture)
@@ -20,4 +20,57 @@ h2.addEventListener("click",
 (event) =>{
     console.log(event.target);
     event.target.textContent = " Again come "; 
-})
+});
+
+// The below code use for serever load or not i want the proccess is over or not 
+
+document.addEventListener
+    ("readystatechange",(event)=>{
+        if(event.target.readyState === "complete"){
+            console.log("complete");
+            App();
+        }
+
+    }
+)
+
+// output : complete
+
+
+
+const App = () =>{
+    const view = document.querySelector("#view2");
+    const div = view.querySelector("div");
+    const h2 = div.querySelector("h2");
+
+    view.addEventListener("click", (event)=>
+    {
+        view.style.backgroundColor = "purple";
+
+    })
+
+    div.addEventListener("click", (event)=>
+    {
+        event.stopPropagation();  // explain below
+        div.style.backgroundColor = "blue";
+
+    })
+
+    h2.addEventListener("click", (event)=>
+    {
+        event.stopPropagation(); // here why use this because when i click the text chage all the above colors so stop this then change only the text here
+        event.target.textContent = "New year";
+
+    })
+
+};
+
+
+
+
+
+
+
+
+
+
